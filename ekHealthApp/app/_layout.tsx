765bcbb5b3691b119dfd1ff15a9e2efb35517309
musengwa himoonga <main.musengwa@gmail.com>
2026-03-02 21:50:43 +0200

    fixed login

--- ekHealthApp/app/_layout.tsx
+++ ekHealthApp/app/_layout.tsx
@@ -1,5 +1,6 @@
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { Stack } from 'expo-router';
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import 'react-native-reanimated';
 
@@ -14,9 +15,12 @@ export default function RootLayout() {
 
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
+      <Stack screenOptions={{
+        headerShown: false,
+      }}>
+        <Stack.Screen name="landing" />
+        <Stack.Screen name="(tabs)" />
+        <Stack.Screen name="doctor" />
       </Stack>
       <StatusBar style="auto" />
     </ThemeProvider>
